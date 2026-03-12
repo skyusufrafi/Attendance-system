e7b4f346086a4746185959dae2c413226d0ed2e4
skyusufrafi <msyusuf932@gmail.com>
2026-03-12 13:05:17 +0530

    Delete models/session.js

--- models/session.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); // <--- ADD THIS HERE TOO
-
-const SessionSchema = new mongoose.Schema({
-    uniqueId: String,
-    createdAt: { type: Date, default: Date.now, expires: 120 }
-});
-
-module.exports = mongoose.model('Session', SessionSchema);
