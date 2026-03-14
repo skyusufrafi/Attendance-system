21ab67cec1e6edb586d9ec98524753f41e381787
skyusufrafi <msyusuf932@gmail.com>
2026-03-14 21:07:03 +0530

    Delete models/Session.js

--- models/Session.js
+++
@@ -1,8 +0,0 @@
-// Session.js
-const SessionSchema = new mongoose.Schema({
-    teacherEmail: String,
-    lectureName: String,
-    uniqueId: String,
-    teacherLocation: { lat: Number, lng: Number },
-    createdAt: { type: Date, default: Date.now }
-});
